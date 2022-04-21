f27cc257507a85d81d0fdb5b40e2fa27fafe3349
Andrew Thresher <thresh3@me.com>
2022-04-21 18:13:33 -0500

    Models templates

---
+++ server/models/index.ts
@@ -0,0 +1,7 @@
+import { Sequelize } from 'sequelize';
+
+const initModels = async (sequelize: Sequelize) => {
+  // Init models here
+};
+
+export { initModels };
---
+++ server/models/paranoid.ts
@@ -0,0 +1,18 @@
+import { InitOptions, Model, Sequelize } from 'sequelize';
+
+class ParanoidModel<Data, CreationData> extends Model<
+  Data,
+  Omit<CreationData, 'createdAt' | 'updatedAt'>
+> {
+  declare readonly createdAt: Date;
+  declare readonly updatedAt: Date;
+  declare readonly deletedAt: Date | null;
+
+  public static initOptions = (connection: Sequelize): Partial<InitOptions> => ({
+    sequelize: connection,
+    timestamps: true,
+    paranoid: true,
+  });
+}
+
+export default ParanoidModel;
