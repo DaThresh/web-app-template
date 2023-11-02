6ae4ba6fd131dc3829c41cd0d8506a389695e6cb
Andrew Thresher <thresh3@me.com>
2023-11-02 10:19:09 -0500

    feat: utilize react 18 render

--- client/index.tsx
+++ client/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.createElement('div');
 rootElement.id = 'root';
 document.body.appendChild(rootElement);
+const root = createRoot(rootElement);
 
-render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+  </React.StrictMode>
 );
