99a2a47844d2510343a146f30945a43f141960f6
Andrew Thresher <thresh3@me.com>
2023-11-02 09:07:05 -0500

    revert: image minimizer plugin

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,8 +1,7 @@
-import { merge } from 'webpack-merge';
-import common from './webpack.common';
-import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import TerserPlugin from 'terser-webpack-plugin';
+import { merge } from 'webpack-merge';
+import common from './webpack.common';
 
 export default merge(common, {
   mode: 'production',
@@ -10,23 +9,6 @@ export default merge(common, {
   optimization: {
     usedExports: true,
     minimize: true,
-    minimizer: [
-      new TerserPlugin(),
-      new CssMinimizerPlugin(),
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [
-              'imagemin-gifsicle',
-              'imagemin-jpegtran',
-              'imagemin-optipng',
-              'imagemin-svgo',
-            ],
-          },
-        },
-        loader: false,
-      }),
-    ],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
 });
